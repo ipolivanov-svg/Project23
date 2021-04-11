7191ddb72684317a74a2232a3547bfa867e6a473
Ivan P <polivanov97ivan@gmail.com>
2021-04-11 16:45:48 +0200

    Level2 with rocks

---
+++ Assets/Scripts/SpawnManagerL1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class SpawnManagerL1 : MonoBehaviour
+{
+    [Header("Parameters")]
+    [SerializeField] private float _delay = 1f;
+    [SerializeField] private GameObject _rockPrefab;
+    [SerializeField] private float _nextRock = -1f;
+    [SerializeField] private float _rockRate = 0.85f;
+
+    //the mode that activates an infinite spawning; always true
+    private bool _spawningON = true;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        StartCoroutine(SpawnSystem());
+    }
+
+    IEnumerator SpawnSystem()
+    {
+        while (_spawningON && Time.time > _nextRock) //condition
+                                                     //if while (true) creates the infinite loop
+        {
+            _nextRock = Time.time + _rockRate;
+            //TODO: я пока хз, что делать с координатами - они фиксированные, для каждой точки придется вручную менять       
+            Instantiate(_rockPrefab, new Vector3(-9.8f, 1f, -10.16f), Quaternion.identity);
+            //wait for 2 seconds
+            yield return new WaitForSeconds(_delay);
+        }
+    }
+}
